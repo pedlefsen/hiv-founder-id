143c8fe2ae7102e09933d5187d0e99ebfd2e88fd
Ted <tholzman@fhcrc.org>
2015-12-04 14:53:28 -0800

    fix error in reRun

--- reRunAllFailed.sh
+++ reRunAllFailed.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for patient in `find /fh/fast/edlefsen_p/bakeoff/analysis_sequences/raw/v3/1m -maxdepth 1 -name "*.err" -size +1000c -exec basename {} .err \;`
+for patient in `find ${1} -maxdepth 1 -name "*.err" -size +1000c -exec basename {} .err \;`
 do 
    sbatch -JhifCEPF${patient} -N1 -t 04:00 --mail-user=tholzman --mail-type=FAIL ./runList.bash ${1} $patient 
 done
