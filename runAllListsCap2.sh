431a87c1df4d1d1566c8437794c97233da3539eb
Ted <tholzman@fhcrc.org>
2015-12-04 12:53:57 -0800

    add fail-fixer.  Only send mail on failures

--- runAllListsCap2.sh
+++ runAllListsCap2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for patient in  `ls -c1 ${1}/*.fasta | cut -d_ -f5 | sort -u | egrep "^[0-9]+"`
 do 
-   sbatch -Jhfi_${patient} -N1 -t 10:00 --mail-user=tholzman --mail-type=ALL ./runList2.bash ${1} $patient 
+   sbatch -Jhfi_${patient} -N1 -t 10:00 --mail-user=tholzman --mail-type=FAIL ./runList2.bash ${1} $patient 
 done
