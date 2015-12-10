0bb001dd82e84d62993bd9293e18b1aa75b5a9e0
Paul T. Edlefsen <pedlefsen@gmail.com>
2015-12-10 05:27:35 -0800

    yes

--- evaluateAllFoundersFromInferCap.sh
+++ evaluateAllFoundersFromInferCap.sh
@@ -3,5 +3,5 @@ mkdir ${3}
 for patient in  `ls -c1 ${1}/*.fasta | cut -d_ -f5 | sort -u | egrep "^[0-9]+" | uniq`
 do
 echo ${patient}
-   ./evaluateFoundersFromInfer.bash ${1} ${2} ${3} ${patient} &
+   ./evaluateFoundersFromInferCap.bash ${1} ${2} ${3} ${patient} &
 done
