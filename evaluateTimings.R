b4ed0fbe48ed10921ddea13b9399535686802a70
Paul T. Edlefsen <pedlefse@fredhutch.org>
2015-12-14 06:33:45 -0800

    results compilation

--- evaluateTimings.R
+++ evaluateTimings.R
@@ -42,7 +42,7 @@ timings.results.by.study.and.time <-
              names( days.since.infection ) <- sample.dates.in[ , "ptid" ];
 
              ## identify-founder results
-             results.in <- read.delim( paste( paste( "/fh/fast/edlefsen_p/bakeoff_analysis_results/raw/", the.study, "/", the.time, "/identify_founders.tab", sep = "" ) ), sep = "\t" );
+             results.in <- read.delim( paste( paste( "/fh/fast/edlefsen_p/bakeoff_merged_analysis_sequences/raw_fixed/", the.study, "/", the.time, "/identify_founders.tab", sep = "" ) ), sep = "\t" );
              # Hack to fix a bug in which the colnames don't have "multifounder." on them (but instead, because of how read.delim works, they have ".1" at the end).
              colnames( results.in ) <- gsub( "^(.+)\\.1$", "multifounder.\\1", colnames( results.in ) );
              results <- as.matrix( results.in );
@@ -57,7 +57,7 @@ timings.results.by.study.and.time <-
              results <- results[ , identify.founders.date.estimates, drop = FALSE ];
 
              ## Add to results: "infer" results.
-             infer.results.directories <- dir( paste( "/fh/fast/edlefsen_p/bakeoff/analysis_sequences/raw/", the.study, "/", the.time, sep = "" ), "founder-inference-bakeoff_", full.name = TRUE );
+             infer.results.directories <- dir( paste( "/fh/fast/edlefsen_p/bakeoff_merged_analysis_sequences/raw_fixed/", the.study, "/", the.time, sep = "" ), "founder-inference-bakeoff_", full.name = TRUE );
              infer.results.files <- sapply( infer.results.directories, dir, "outtoi.csv", full.name = TRUE );
              infer.results.list <-
                  lapply( unlist( infer.results.files ), function( .file ) {
@@ -98,7 +98,7 @@ timings.results.by.study.and.time <-
                        .file.short.nosuffix <-
                            gsub( "^([^\\.]+)(\\..+)?$", "\\1", .file.short, perl = TRUE );
                        .file.converted <-
-                           paste( "/fh/fast/edlefsen_p/bakeoff_analysis_results/raw/", the.study, "/1m6m/", .file.short.nosuffix, ".anc2tsv.tab", sep = "" );
+                           paste( "/fh/fast/edlefsen_p/bakeoff_analysis_results/raw_fixed/", the.study, "/1m6m/", .file.short.nosuffix, ".anc2tsv.tab", sep = "" );
                        # convert it.
                        system( paste( "./anc2tsv.sh", .file, ">", .file.converted ) );
                        stopifnot( file.exists( .file.converted ) );
@@ -150,302 +150,59 @@ results.tables <-
         return( .rv );
     } );
 names( results.tables ) <- studies;
-lapply( studies, function( the.study ) {
-.tbl <- do.call( cbind, lapply( timings.results.by.study.and.time[[ the.study ]], function( .lst.by.time ) {
-  unlist( list( Bias = .lst.by.time[[ "bias" ]], RMSE = .lst.by.time[[ "rmse" ]] ) )
-} ) );
-return( .tbl );
-} );
-names( results.tables ) <- studies;
 results.tables
 # $nflg
-#                                                       1m         6m      1m6m
-# Bias.PFitter.time.est                          43.250000  -26.36986  91.62162
-# Bias.Synonymous.PFitter.time.est              -23.625000 -145.23288 -11.10811
-# Bias.multifounder.PFitter.time.est              6.137255  -66.92593  21.83333
-# Bias.multifounder.Synonymous.PFitter.time.est -30.843137 -151.38889 -25.23333
-# Bias.Infer                                     88.531250   82.11111  72.65000
-# Bias.Anchre.r2t                               120.393666  121.64161  75.26316
-# Bias.Anchre.bst                                28.133187   29.92886 291.21053
-# RMSE.PFitter.time.est                          61.020331   91.80863 135.82308
-# RMSE.Synonymous.PFitter.time.est               16.371161   28.42296  32.30822
-# RMSE.multifounder.PFitter.time.est              4.607038   12.27091  70.30431
-# RMSE.multifounder.Synonymous.PFitter.time.est  43.250000  -26.36986  20.79072
-# RMSE.Infer                                    -23.625000 -145.23288  24.31433
-# RMSE.Anchre.r2t                                 6.137255  -66.92593 174.86371
-# RMSE.Anchre.bst                               -30.843137 -151.38889 714.73403
-# 
-# $v3
-#                                                       1m         6m
-# Bias.PFitter.time.est                         179.200000  139.27778
-# Bias.Synonymous.PFitter.time.est               -4.200000 -120.77778
-# Bias.multifounder.PFitter.time.est              5.900000  -25.88889
-# Bias.multifounder.Synonymous.PFitter.time.est -40.900000 -144.77778
-# Bias.Infer                                     77.450000   93.38889
-# Bias.Anchre.r2t                               351.525937  203.41953
-# Bias.Anchre.bst                               113.694234   73.17309
-# RMSE.PFitter.time.est                          76.814815  156.67121
-# RMSE.Synonymous.PFitter.time.est               35.936933   55.60528
-# RMSE.multifounder.PFitter.time.est              5.296225   12.71469
-# RMSE.multifounder.Synonymous.PFitter.time.est 179.200000  139.27778
-# RMSE.Infer                                     -4.200000 -120.77778
-# RMSE.Anchre.r2t                                 5.900000  -25.88889
-# RMSE.Anchre.bst                               -40.900000 -144.77778
-#                                                        1m6m
-# Bias.PFitter.time.est                            259.411765
-# Bias.Synonymous.PFitter.time.est                   2.705882
-# Bias.multifounder.PFitter.time.est               123.411765
-# Bias.multifounder.Synonymous.PFitter.time.est    -18.588235
-# Bias.Infer                                       101.352941
-# Bias.Anchre.r2t                                  695.529412
-# Bias.Anchre.bst                               321256.235294
-# RMSE.PFitter.time.est                            360.149632
-# RMSE.Synonymous.PFitter.time.est                  96.109810
-# RMSE.multifounder.PFitter.time.est               319.683644
-# RMSE.multifounder.Synonymous.PFitter.time.est     72.516428
-# RMSE.Infer                                        24.264020
-# RMSE.Anchre.r2t                                 1604.720736
-# RMSE.Anchre.bst                               333778.988566
-
-# ------- FOR THE RECORD ---------
-# timings.results.by.study.and.time
-# $nflg
 # $nflg$`1m`
-# $nflg$`1m`$bias
-# $nflg$`1m`$bias$PFitter.time.est
-# [1] 43.25
-# 
-# $nflg$`1m`$bias$Synonymous.PFitter.time.est
-# [1] -23.625
-# 
-# $nflg$`1m`$bias$multifounder.PFitter.time.est
-# [1] 6.137255
-# 
-# $nflg$`1m`$bias$multifounder.Synonymous.PFitter.time.est
-# [1] -30.84314
-# 
-# $nflg$`1m`$bias$Infer
-# [1] 88.53125
-# 
-# 
-# $nflg$`1m`$rmse
-# $nflg$`1m`$rmse$PFitter.time.est
-# [1] 120.3937
-# 
-# $nflg$`1m`$rmse$Synonymous.PFitter.time.est
-# [1] 28.13319
-# 
-# $nflg$`1m`$rmse$multifounder.PFitter.time.est
-# [1] 61.02033
-# 
-# $nflg$`1m`$rmse$multifounder.Synonymous.PFitter.time.est
-# [1] 16.37116
-# 
-# $nflg$`1m`$rmse$Infer
-# [1] 4.607038
-# 
-# 
+#                                          bias      rmse    
+# PFitter.time.est                         39.39216  115.1498
+# Synonymous.PFitter.time.est              -23.13725 28.43942
+# multifounder.PFitter.time.est            6.137255  61.02033
+# multifounder.Synonymous.PFitter.time.est -30.84314 16.37116
+# Infer                                    86.92593  5.128547
 # 
 # $nflg$`6m`
-# $nflg$`6m`$bias
-# $nflg$`6m`$bias$PFitter.time.est
-# [1] -26.36986
-# 
-# $nflg$`6m`$bias$Synonymous.PFitter.time.est
-# [1] -145.2329
-# 
-# $nflg$`6m`$bias$multifounder.PFitter.time.est
-# [1] -66.92593
-# 
-# $nflg$`6m`$bias$multifounder.Synonymous.PFitter.time.est
-# [1] -151.3889
-# 
-# $nflg$`6m`$bias$Infer
-# [1] 82.11111
-# 
-# 
-# $nflg$`6m`$rmse
-# $nflg$`6m`$rmse$PFitter.time.est
-# [1] 121.6416
-# 
-# $nflg$`6m`$rmse$Synonymous.PFitter.time.est
-# [1] 29.92886
-# 
-# $nflg$`6m`$rmse$multifounder.PFitter.time.est
-# [1] 91.80863
-# 
-# $nflg$`6m`$rmse$multifounder.Synonymous.PFitter.time.est
-# [1] 28.42296
-# 
-# $nflg$`6m`$rmse$Infer
-# [1] 12.27091
-# 
-# 
+#                                          bias      rmse    
+# PFitter.time.est                         -23.27778 124.5701
+# Synonymous.PFitter.time.est              -142.7407 31.42117
+# multifounder.PFitter.time.est            -66.92593 91.80863
+# multifounder.Synonymous.PFitter.time.est -151.3889 28.42296
+# Infer                                    85.53333  13.15222
 # 
 # $nflg$`1m6m`
-# $nflg$`1m6m`$bias
-# $nflg$`1m6m`$bias$PFitter.time.est
-# [1] 91.62162
-# 
-# $nflg$`1m6m`$bias$Synonymous.PFitter.time.est
-# [1] -11.10811
-# 
-# $nflg$`1m6m`$bias$multifounder.PFitter.time.est
-# [1] 21.83333
-# 
-# $nflg$`1m6m`$bias$multifounder.Synonymous.PFitter.time.est
-# [1] -25.23333
-# 
-# $nflg$`1m6m`$bias$Infer
-# [1] 76.05
-# 
-# $nflg$`1m6m`$bias$Anchre.r2t
-# [1] 12
-# 
-# $nflg$`1m6m`$bias$Anchre.bst
-# [1] 100.7059
-# 
-# 
-# $nflg$`1m6m`$rmse
-# $nflg$`1m6m`$rmse$PFitter.time.est
-# [1] 135.8231
-# 
-# $nflg$`1m6m`$rmse$Synonymous.PFitter.time.est
-# [1] 32.30822
-# 
-# $nflg$`1m6m`$rmse$multifounder.PFitter.time.est
-# [1] 70.30431
-# 
-# $nflg$`1m6m`$rmse$multifounder.Synonymous.PFitter.time.est
-# [1] 20.79072
-# 
-# $nflg$`1m6m`$rmse$Infer
-# [1] 21.87157
-# 
-# $nflg$`1m6m`$rmse$Anchre.r2t
-# [1] 32.39792
-# 
-# $nflg$`1m6m`$rmse$Anchre.bst
-# [1] 323.2957
-# 
-# 
+#                                          bias      rmse    
+# PFitter.time.est                         92.3      130.6546
+# Synonymous.PFitter.time.est              -8.533333 34.35889
+# multifounder.PFitter.time.est            21.83333  70.30431
+# multifounder.Synonymous.PFitter.time.est -25.23333 20.79072
+# Infer                                    73.6875   25.60916
+# Anchre.r2t                               53.4375   140.108 
+# Anchre.bst                               291.9375  757.6369
 # 
 # 
 # $v3
 # $v3$`1m`
-# $v3$`1m`$bias
-# $v3$`1m`$bias$PFitter.time.est
-# [1] 179.2
-# 
-# $v3$`1m`$bias$Synonymous.PFitter.time.est
-# [1] -4.2
-# 
-# $v3$`1m`$bias$multifounder.PFitter.time.est
-# [1] 5.9
-# 
-# $v3$`1m`$bias$multifounder.Synonymous.PFitter.time.est
-# [1] -40.9
-# 
-# $v3$`1m`$bias$Infer
-# [1] 77.45
-# 
-# 
-# $v3$`1m`$rmse
-# $v3$`1m`$rmse$PFitter.time.est
-# [1] 351.5259
-# 
-# $v3$`1m`$rmse$Synonymous.PFitter.time.est
-# [1] 113.6942
-# 
-# $v3$`1m`$rmse$multifounder.PFitter.time.est
-# [1] 76.81481
-# 
-# $v3$`1m`$rmse$multifounder.Synonymous.PFitter.time.est
-# [1] 35.93693
-# 
-# $v3$`1m`$rmse$Infer
-# [1] 5.296225
-# 
-# 
+#                                          bias   rmse    
+# PFitter.time.est                         154    341.5987
+# Synonymous.PFitter.time.est              -21.9  61.98209
+# multifounder.PFitter.time.est            -1     71.1248 
+# multifounder.Synonymous.PFitter.time.est -41.05 31.57693
+# Infer                                    77.45  5.296225
 # 
 # $v3$`6m`
-# $v3$`6m`$bias
-# $v3$`6m`$bias$PFitter.time.est
-# [1] 139.2778
-# 
-# $v3$`6m`$bias$Synonymous.PFitter.time.est
-# [1] -120.7778
-# 
-# $v3$`6m`$bias$multifounder.PFitter.time.est
-# [1] -25.88889
-# 
-# $v3$`6m`$bias$multifounder.Synonymous.PFitter.time.est
-# [1] -144.7778
-# 
-# $v3$`6m`$bias$Infer
-# [1] 93.38889
-# 
-# 
-# $v3$`6m`$rmse
-# $v3$`6m`$rmse$PFitter.time.est
-# [1] 203.4195
-# 
-# $v3$`6m`$rmse$Synonymous.PFitter.time.est
-# [1] 73.17309
-# 
-# $v3$`6m`$rmse$multifounder.PFitter.time.est
-# [1] 156.6712
-# 
-# $v3$`6m`$rmse$multifounder.Synonymous.PFitter.time.est
-# [1] 55.60528
-# 
-# $v3$`6m`$rmse$Infer
-# [1] 12.71469
-# 
-# 
+#                                          bias      rmse    
+# PFitter.time.est                         139.2778  203.4195
+# Synonymous.PFitter.time.est              -120.7778 73.17309
+# multifounder.PFitter.time.est            -25.88889 156.6712
+# multifounder.Synonymous.PFitter.time.est -144.7778 55.60528
+# Infer                                    93.38889  12.71469
 # 
 # $v3$`1m6m`
-# $v3$`1m6m`$bias
-# $v3$`1m6m`$bias$PFitter.time.est
-# [1] 259.4118
-# 
-# $v3$`1m6m`$bias$Synonymous.PFitter.time.est
-# [1] 2.705882
-# 
-# $v3$`1m6m`$bias$multifounder.PFitter.time.est
-# [1] 123.4118
-# 
-# $v3$`1m6m`$bias$multifounder.Synonymous.PFitter.time.est
-# [1] -18.58824
-# 
-# $v3$`1m6m`$bias$Infer
-# [1] 101.3529
-# 
-# $v3$`1m6m`$bias$Anchre.r2t
-# [1] 695.5294
-# 
-# $v3$`1m6m`$bias$Anchre.bst
-# [1] 321256.2
-# 
-# 
-# $v3$`1m6m`$rmse
-# $v3$`1m6m`$rmse$PFitter.time.est
-# [1] 360.1496
-# 
-# $v3$`1m6m`$rmse$Synonymous.PFitter.time.est
-# [1] 96.10981
-# 
-# $v3$`1m6m`$rmse$multifounder.PFitter.time.est
-# [1] 319.6836
-# 
-# $v3$`1m6m`$rmse$multifounder.Synonymous.PFitter.time.est
-# [1] 72.51643
-# 
-# $v3$`1m6m`$rmse$Infer
-# [1] 24.26402
-# 
-# $v3$`1m6m`$rmse$Anchre.r2t
-# [1] 1604.721
-# 
-# $v3$`1m6m`$rmse$Anchre.bst
-# [1] 333779
+#                                          bias      rmse    
+# PFitter.time.est                         259.2105  339.6857
+# Synonymous.PFitter.time.est              6.210526  96.55947
+# multifounder.PFitter.time.est            112.8421  303.213 
+# multifounder.Synonymous.PFitter.time.est -22.57895 69.40807
+# Infer                                    96.57895  28.13127
+# Anchre.r2t                               620.6842  1530.37 
+# Anchre.bst                               325964    337789.5
+
