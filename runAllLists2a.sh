cfb0b287b6c6d911df2f2c38955ac1e7dd54ff03
Ted <tholzman@fhcrc.org>
2016-02-26 11:07:51 -0800

    pull phillip's code? Add more pipeline scripts

---
+++ runAllLists2a.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+for patient in  `ls -c1 ${1}/*.fasta | egrep --only "_[0-9]{5,}_" | tr -d "_" | sort -u`
+do 
+   sbatch -Jhfi_${patient} -N1 -t 04:00 --mail-user=tholzman --mail-type=FAIL ./runList2a.bash ${1} $patient 
+done
