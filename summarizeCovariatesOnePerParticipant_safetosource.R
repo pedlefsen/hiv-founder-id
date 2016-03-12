291e04642c1d376490501d08eeb76392dca988cc
Paul T. Edlefsen <pedlefsen@gmail.com>
2016-03-11 23:42:48 -0800

    bugfix

--- summarizeCovariatesOnePerParticipant_safetosource.R
+++ summarizeCovariatesOnePerParticipant_safetosource.R
@@ -1,32 +1,44 @@
 # hello.
 
 summarizeCovariatesOnePerParticipant <- function ( results ) {
-
-                   results.covars.colnames <- c( "num.seqs", "num.diversity.seqs", "diversity", "inf.sites", "priv.sites", "inf.to.priv.ratio", "mean.entropy", "sd.entropy", "inf.sites.clusters", "InSites.founders", "StarPhy.founders", "multifounder.Synonymous.PFitter.is.poisson" );
-
-                   ## Setting up.  Add a column for the coefficients that are used by the daysFromLambda function.
-                   mutation.rate.coefs <-
-                       sapply( 1:length( days.est.colnames.nb ), function( days.est.col.i ) {
-                             apply( results, 1, function( .row ) {
-                                 if( is.na( .row[ days.est.colnames.nb[ days.est.col.i ] ] ) ) {
-                                     return( NA );
-                                 }
-                                 return( daysFromLambda.coefficient.of.inverse.epsilon( .row[ lambda.est.colnames[ days.est.col.i ] ], .row[ days.est.colnames.nb[ days.est.col.i ] ] ) );
-                             } );
-                         } );
-                   colnames( mutation.rate.coefs ) <-
-                       gsub( "(?:days|time)\\.est", "mut.rate.coef", days.est.colnames );
-
-                   mutation.rate.coefs.totalbases <- days.est.nseq*days.est.nb;
-                   colnames( mutation.rate.coefs.totalbases ) <-
-                       gsub( "(?:days|time)\\.est", "mut.rate.coef.totalbases", days.est.colnames );
-
-                   covars <-
-                       cbind(
-                           results[ , results.covars.colnames, drop = FALSE ],
-                           mutation.rate.coefs,
-                           mutation.rate.coefs.totalbases
-                       );
+  
+    days.colnames <- c( grep( "time", colnames( results ), value = T ), grep( "days", colnames( results ), value = T ) );
+    
+    days.est.colnames <- grep( "est", days.colnames, value = TRUE );
+    days.est <- results[ , days.est.colnames, drop = FALSE ];
+    lambda.est.colnames <-
+        gsub( "PFitter\\.lambda\\.est", "PFitter.lambda", gsub( "(?:days|time|fits)", "lambda", days.est.colnames, perl = TRUE ) );
+    stopifnot( all( lambda.est.colnames %in% colnames( results ) ) );
+    days.est.colnames.nb <- gsub( "[^\\.]+\\.Star[Pp]hy", "PFitter", gsub( "(?:days|time|fits).*$", "nbases", days.est.colnames, perl = TRUE ) );
+    days.est.nb <- results[ , days.est.colnames.nb, drop = FALSE ];
+    days.est.colnames.nseq <- gsub( "[^\\.]+\\.Star[Pp]hy", "PFitter", gsub( "(?:days|time|fits).*$", "nseq", days.est.colnames, perl = TRUE ) );
+    days.est.nseq <- results[ , days.est.colnames.nseq, drop = FALSE ];
+    
+    results.covars.colnames <- c( "num.seqs", "num.diversity.seqs", "diversity", "inf.sites", "priv.sites", "inf.to.priv.ratio", "mean.entropy", "sd.entropy", "inf.sites.clusters", "InSites.founders", "StarPhy.founders", "multifounder.Synonymous.PFitter.is.poisson" );
+    
+    ## Setting up.  Add a column for the coefficients that are used by the daysFromLambda function.
+    mutation.rate.coefs <-
+        sapply( 1:length( days.est.colnames.nb ), function( days.est.col.i ) {
+              apply( results, 1, function( .row ) {
+                  if( is.na( .row[ days.est.colnames.nb[ days.est.col.i ] ] ) ) {
+                      return( NA );
+                  }
+                  return( daysFromLambda.coefficient.of.inverse.epsilon( .row[ lambda.est.colnames[ days.est.col.i ] ], .row[ days.est.colnames.nb[ days.est.col.i ] ] ) );
+              } );
+          } );
+    colnames( mutation.rate.coefs ) <-
+        gsub( "(?:days|time)\\.est", "mut.rate.coef", days.est.colnames );
+    
+    mutation.rate.coefs.totalbases <- days.est.nseq*days.est.nb;
+    colnames( mutation.rate.coefs.totalbases ) <-
+        gsub( "(?:days|time)\\.est", "mut.rate.coef.totalbases", days.est.colnames );
+    
+    covars <-
+        cbind(
+            results[ , results.covars.colnames, drop = FALSE ],
+            mutation.rate.coefs,
+            mutation.rate.coefs.totalbases
+        );
     
     # Five cases: diversity, entropy, single, mut.rate.coef, and the rest ("max").
     # Case 1: for diversity, we create combined measures weighted using num.diversity.seqs.
