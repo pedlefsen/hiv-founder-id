a52e55eef05448542469920563d82a7b733ca393
Paul <pedlefse@scharp.org>
2016-03-18 17:18:36 -0700

    added splitting of the results by original/uncalibrated result, glm, and lasso

--- getFilteredResultsTables_safetosource.R
+++ getFilteredResultsTables_safetosource.R
@@ -1,7 +1,35 @@
+repeatedRowsToColumns <- function ( the.matrix, pattern ) {
+    rownames.sans.patterns <- gsub( paste( "^(.*?)", pattern, "(.*)$", sep = "" ), "\\1\\2", rownames( the.matrix ) )
+    pattern.matches.by.row <- gsub( paste( "^.*?(", pattern, ").*$", sep = "" ), "\\1", rownames( the.matrix ) );
+    pattern.matches.by.row[ rownames.sans.patterns == pattern.matches.by.row ] <- "";
+
+    result.submatrices <- lapply( unique( pattern.matches.by.row ), function ( .pattern ) {
+        .rm <- the.matrix[ pattern.matches.by.row == .pattern, , drop = FALSE ];
+        colnames( .rm ) <- paste( .pattern, colnames( the.matrix ), sep = "" );
+        rownames( .rm ) <-
+            gsub( paste( "^(.*?)", pattern, "(.*)$", sep = "" ), "\\1\\2", rownames( .rm ) );
+        return( .rm );
+    } );
+    names( result.submatrices ) <- unique( pattern.matches.by.row );
+    result.matrix <- matrix( NA, nrow = length( unique( rownames.sans.patterns ) ), ncol = length( result.submatrices ) * ncol( the.matrix ) );
+    rownames( result.matrix ) <- unique( rownames.sans.patterns );
+    colnames( result.matrix ) <- sapply( colnames( the.matrix ), function( .suffix ) { paste( names( result.submatrices ), .suffix, sep = "" ); } );
+    .result.ignored <- lapply( result.submatrices, function( .result.submatrix ) {
+        lapply( rownames( .result.submatrix ), function( .rowname ) {
+          result.matrix[ .rowname, colnames( .result.submatrix ) ] <<-
+              .result.submatrix[ .rowname, ];
+          return( NULL );
+        } )
+        return( NULL );
+    } );
+    return( result.matrix );
+} # repeatedRowsToColumns ( .. )
+
 ### Read results tables.
 ## setting the.time to "1m.6m" will return pooled results over those times (but note current limitation that the bounds will be for either 1m (5weeks) or 6m (30weeks) but presently not use-the-right-bounds-matching-the-data-timepoints).
 getFilteredResultsTables <- function (
-    out.tab.file.suffix, the.region, the.time, the.bounds.type = "unbounded", to.region = NULL, results.dirname = "raw_edited_20160216", zeroNAs = FALSE, sort.column = "rmse" ) {
+    out.tab.file.suffix, the.region, the.time, the.bounds.type = "unbounded", to.region = NULL, results.dirname = "raw_edited_20160216", zeroNAs = FALSE, sort.column = "rmse"
+) {
     if( is.null( to.region ) || is.na( to.region ) ) {
         ## if to.region is not defined then it means we should use the single-region results.
         infile.results <- paste( "/fh/fast/edlefsen_p/bakeoff_analysis_results/", results.dirname, "/", the.region, "/", the.time, "/", the.bounds.type, out.tab.file.suffix, sep = "" );
@@ -30,15 +58,17 @@ getFilteredResultsTables <- function (
     } else if( the.time == "1m.6m" ) {
         the.times.it.aint <- c( "\\.5weeks", "\\.30weeks", "20weeks", "1m5weeks_6m30weeks" );
     }
-    results.filtered <- results[ grep( paste( c( "deterministic", the.times.it.aint ), collapse = "|" ), rownames( results ), invert = TRUE ), , drop = FALSE ];
-    
+    ## Also exclude deterministic bounds
+    ## Also exclude "lower" and "upper" versions of results, which appear to be redundant.
+    results.filtered <- results[ grep( paste( c( "lower", "upper", "deterministic", the.times.it.aint ), collapse = "|" ), rownames( results ), invert = TRUE ), , drop = FALSE ];
+
     if( !is.null( sort.column ) && ( length( sort.column ) > 0 ) && !is.na( sort.column ) ) {
         stopifnot( length( sort.column ) == 1 );
         stopifnot( sort.column %in% names( results.filtered ) );
         results.filtered.sorted <-
             results.filtered[ order( results.filtered[[ sort.column ]] ), , drop = FALSE ];
-        return( results.filtered.sorted );
+        return( repeatedRowsToColumns( results.filtered.sorted, pattern = "(?:glm|lasso)\\.validation\\.results\\." ) );
     }
-    
-    return( results.filtered );
+
+    return( repeatedRowsToColumns( results.filtered, pattern = "(?:glm|lasso)\\.validation\\.results\\." ) );
 } # getFilteredResultsTables (..)
