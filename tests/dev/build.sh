cf8cd32a9486728459d7837b9a1663050f7a7be8
Phillip Labuschagne <philliplab@gmail.com>
2018-11-23 08:55:04 +0200

    pacakge test results into a data.frame

---
+++ tests/dev/build.sh
@@ -0,0 +1,2 @@
+R -q -e "rmarkdown::render('/home/phillipl/projects/hiv-founder-id/code/hiv-founder-id/tests/dev/dev_tests.Rmd', output_dir = '/home/phillipl/projects/hiv-founder-id/code/hiv-founder-id/tests/dev')"
+firefox /home/phillipl/projects/hiv-founder-id/code/hiv-founder-id/tests/dev/dev_tests.html
