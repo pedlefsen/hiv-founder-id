c8fddbbdcfa5bc8012fae260572318219c2e6830
Phillip Labuschagne <philliplab@gmail.com>
2018-11-23 11:41:41 +0200

    ADD check: command file match test's command

--- tests/commands/test_001.sh
+++ tests/commands/test_001.sh
@@ -1,5 +1,3 @@
-
-cd /home/phillipl/projects/hiv-founder-id/code/hiv-founder-id
+ cd /home/phillipl/projects/hiv-founder-id/code/hiv-founder-id
 rm -r tests/tmp/test_001
-perl identify_founders.pl -CPRFHT -o tests/tmp/test_001 tests/data/ld_seqs.fasta 
-
+perl identify_founders.pl -CPRFHT -o tests/tmp/test_001 tests/data/ld_seqs.fasta
