27086172ccc5e98b08cc5d4553878021807f1319
Phillip Labuschagne <philliplab@gmail.com>
2018-11-28 09:50:22 +0200

    ADD functionality for the --run_all_test_command_scripts option

--- tests/test_pipeline.R
+++ tests/test_pipeline.R
@@ -5,36 +5,30 @@ suppressPackageStartupMessages(library(knitr))
 
 option_list <- list(
 
-#make_option("--non_overlapping", 
-#            action = "store_true",
-#            default = FALSE,
-#            help = "If your forwards and reverse reads do not overlap, include this flag. Also, if they do overlap, but you do not want them to be merged, include this flag."),
-
-#make_option("--max_seq",
-#            default = 0,
-#            help = paste("Maximum number of sequences to read from the input files. By default the ",
-#                         "forward and reverse read files produced by a MiSeq are in the same order, ",
-#                         "so just reading out the first 'max_seq' sequences from each file should ",
-#                         "yield 'max_seq' pair-end reads in the dataset.",
-#                         sep = "")),
-
 make_option("--pipeline_dir", help = "Path to the root of the pipeline folder"),
 
 make_option('--print_spec',
             action = 'store_true',
+            default = FALSE,
             help = 'Prints out a list of all the tests in the spec file'),
 
 make_option('--compare_spec_to_command_scripts',
             action = 'store_true',
-            help = 'Compares the command scripts in the commands folder to the test specifications in the test_spec.csv file')
+            default = FALSE,
+            help = 'Compares the command scripts in the commands folder to the test specifications in the test_spec.csv file'),
+
+make_option('--run_all_test_command_scripts',
+            action = 'store_true',
+            default = FALSE,
+            help = 'Generate the call that will produce all the pipeline runs required to perform the testing')
+
 )
 
 opt <- parse_args(OptionParser(option_list = option_list,
   description = 'Script to control the testing for the hiv-founder-pipeline',
   epilogue = 'Example Calls:
 ./test_pipeline.R --pipeline-dir="/home/phillipl/projects/hiv-founder-id/code/hiv-founder-id" --print-spec
-')
-)
+'))
 
 print(opt)
 
@@ -62,4 +56,13 @@ Test Specification File:
   kable(test_spec)
 }
 
+if (opt$run_all_test_command_scripts){
+  cat(paste('
+Due to a weird interaction between R, bash and the pipeline, this script cannot execute the appropriate calls. Copy and paste the following command into a terminal to perform all the runs.
+WARNING: This may take a long time.
+
+cd ', pipeline_dir, '/tests/commands; ./run_all.sh
+
+', sep = ''))
+}
 
