babf596e5d096cd64e7a9f7cd0f8f47c64934042
Phillip Labuschagne <philliplab@gmail.com>
2019-05-31 14:38:20 +0200

    BUG FIX more directories incorrectly specified

--- tests/example_docker.sh
+++ tests/example_docker.sh
@@ -9,7 +9,7 @@ perl /home/docker/hiv-founder-id/identify_founders.pl -PRT -o /home/docker/examp
 perl /home/docker/hiv-founder-id/identify_founders.pl -PRT -o /home/docker/example/example_7 /home/docker/example/example_7.fasta
 
 # collate identify_founders.tab files
-cp /home/docker/example/identify_founders.tab /home/docker/example/identify_founders.tab
+cp        /home/docker/example/example_1/identify_founders.tab    /home/docker/example/identify_founders.tab
 tail -n 1 /home/docker/example/example_2/identify_founders.tab >> /home/docker/example/identify_founders.tab
 tail -n 1 /home/docker/example/example_3/identify_founders.tab >> /home/docker/example/identify_founders.tab
 tail -n 1 /home/docker/example/example_4/identify_founders.tab >> /home/docker/example/identify_founders.tab
@@ -21,4 +21,4 @@ tail -n 1 /home/docker/example/example_7/identify_founders.tab >> /home/docker/e
 
 /home/docker/hiv-founder-id/estimateInfectionTime.R --model_structure=slope --identify_founders.tab=/home/docker/example/identify_founders.tab --estimator=pfitter
 
-/home/docker/hiv-founder-id/estimateInfectionTime.R --model_structure=full --identify_founders.tab=/home/docker/example/identify_founders.tab --vl_file=/home/docker/vl_diff.csv --bounds_file=/home/docker/bounds_diff.csv --estimator=pfitter
+/home/docker/hiv-founder-id/estimateInfectionTime.R --model_structure=full --identify_founders.tab=/home/docker/example/identify_founders.tab --vl_file=/home/docker/example/vl_diff.csv --bounds_file=/home/docker/example/bounds_diff.csv --estimator=pfitter
